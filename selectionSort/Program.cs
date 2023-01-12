f69f6d84c31e231ee24a18bcf581088928a3f8ed
Петр < >
2023-01-12 13:20:31 +0300

    добавлен простой пример реализации сортировки выбором

--- selectionSort/Program.cs
+++ selectionSort/Program.cs
@@ -1,9 +1,25 @@
-﻿// 1 2 3 4 5 1 2 3
+﻿void SortSelection(int[] collection)
+{
+    int size = collection.Length;
 
-// Min > Max
-// v
-// 0 1 2 3 4 5 6 7
-// 7 6 3 4 5 1 2 3  > было
-// 1 6 3 4 5 7 2 3  > стало
-// 1 2 3 4 5 7 6 3  > стало 
-// 1 2 3 3 4 5 6 7  > итог 
+    for (int i = 0; i < size - 1; i++)
+    {
+        int pos = i;
+
+        for (int j = i + 1; j < size; j++)
+        {
+            if (collection[j] < collection[pos]) pos = j;
+        }
+        int temp = collection[i];
+        collection[i] = collection[pos];
+        collection[pos] = temp;
+    }
+}
+
+int [] array = {7, 6, 3, 4, 5, 1, 2, 3,};
+
+Console.WriteLine($"[{string.Join(',',array)}]"); 
+
+SortSelection(array);
+
+Console.WriteLine($"[{string.Join(',',array)}]"); 
